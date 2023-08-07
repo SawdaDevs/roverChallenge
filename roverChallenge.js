26083d65f7603b48844da684efcefb82cb3e18ce
SawdaDevs <sawda.onanuga@gmail.com>
2023-08-08 00:37:36 +0100

    tests pass for coordinates

--- roverChallenge.js
+++ roverChallenge.js
@@ -1,10 +1,89 @@
-const rover = (limits, startPosition, movements)=>{
+const rover = (limits, startPosition, movements) => {
 
+    let abc = {
+        description: "First Rover",
+        limits: [5, 5],
+        roverStart: [1, 2, "N"],
+        roverMovements: 'LMLMLMLMM',
+        roverEnd: [1, 3, "N"]
+    }
+    currentPostion = [...startPosition]
 
+    console.log("The rover is starting here:", currentPostion)
 
- const val = [1,3,"N"]
-    return val
+    const faceDirection = (rotation) => {
+        console.log("this is rotation:", rotation, currentPostion[2])
+ 
+        if (currentPostion[2] == "N" && rotation == 'R') {
+            currentPostion[2] = "E"
+            console.log("Now current face is ", currentPostion[2])
+            return;
+        }
 
+        if (currentPostion[2] == "N" && rotation == 'L') {
+            currentPostion[2] = "W"
+            console.log("Now current face is ", currentPostion[2])
+            return
+        }
+        if (currentPostion[2] == "E" && rotation == 'R') {
+            currentPostion[2] = "S"
+            console.log("Now current face is ", currentPostion[2])
+            return
+        }
+        if (currentPostion[2] == "E" && rotation == 'L') {
+            currentPostion[2] = "N"
+            console.log("Now current face is ", currentPostion[2])
+            return;
+        }
+        if (currentPostion[2] == "W" && rotation == 'R') {
+            currentPostion[2] = "N"
+            console.log("Now current face is ", currentPostion[2])
+        }
+        if (currentPostion[2] == "W" && rotation == 'L') {
+            currentPostion[2] = "S"
+            console.log("Now current face is ", currentPostion[2])
+            return;
+        }
+        if (currentPostion[2] == "S" && rotation == 'R') {
+            currentPostion[2] = "W"
+            console.log("Now current face is ", currentPostion[2])
+            return;
+        }
+        if (currentPostion[2] == "S" && rotation == 'L') {
+            currentPostion[2] = "E"
+            console.log("Now current face is ", currentPostion[2])
+            return;
+        }
+    }
+    const moveRover = () => {
+        if (currentPostion[2] == "N") {
+            currentPostion[1] += 1
+        }
+        if (currentPostion[2] == "S") {
+            currentPostion[1] -= 1
+        }
+        if (currentPostion[2] == "E") {
+            currentPostion[0] += 1
+        }
+        if (currentPostion[2] == "W") {
+            currentPostion[0] -= 1
+        }
+    }
+
+    move = movements.split("")
+    console.log(move)
+
+    move.forEach(letter => {
+
+        console.log("letter is:", letter)
+
+        if (letter !== "M") {
+            faceDirection(letter)
+        }
+        else moveRover()
+        console.log("Now we are at", currentPostion)
+    });
+    return currentPostion
 
 }
 exports.rover = rover;
