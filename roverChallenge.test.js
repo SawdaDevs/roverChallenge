c94fd3c9cec2da9f892b7d356d80f4e742898a22
SawdaDevs <sawda.onanuga@gmail.com>
2023-08-21 14:27:15 +0100

    Added tests, all pass

--- roverChallenge.test.js
+++ roverChallenge.test.js
@@ -1,60 +1,107 @@
-const { rover, OutOfBoundsError } = require('./roverChallenge')
+const { rover } = require('./roverChallenge')
 
 inBoundtests = [
     {
-        description:"First Rover",
+        description:"First Rover (given)",
         limits: "5,5",
         roverStart: "1,2,N",
         roverMovements: 'LMLMLMLMM',
         roverEnd:"1,3,N"
     },
     {
-        description:"Second Rover",
+        description:"Second Rover (given)",
         limits: "5,5",
         roverStart: "3,3,E",
         roverMovements: 'MMRMMRMRRM',
         roverEnd:"5,1,E"
     },
     {
-        description:"Third Rover",
+        description:"Third Rover (test boundaries)",
         limits: "10,10",
         roverStart: "0,0,E",
         roverMovements: 'MMMMMMMMMM',
         roverEnd:"10,0,E"
     },
     {
-        description:"Fourth Rover",
+        description:"Fourth Rover (test boundaries)",
         limits: "10,10",
         roverStart: "0,0,N",
         roverMovements: 'MMMMMMMMMM',
         roverEnd:"0,10,N"
     },
+    {
+        description:"Fifth Rover (test negative numbers)",
+        limits: "-5, -5",
+        roverStart: "1,2,N",
+        roverMovements: 'LMLMLMLMM',
+        roverEnd:"1,3,N"
+    },
 
 ]
 outOfBoundsTests = [
     {
-        description:"Fifth Rover",
+        description:"Sixth Rover",
         limits: "5,5",
         roverStart: "1,2,N",
         roverMovements: 'RRMMMMM'
     },
     {
-        description:"Sixth Rover",
+        description:"Seventh Rover",
         limits: "5,5",
         roverStart: "1,2,N",
         roverMovements: 'MMMMMN'
     },
     {
-        description:"Seventh Rover",
+        description:"Eigth Rover",
         limits: "5,5",
         roverStart: "0,0,E",
         roverMovements: 'RRMMMMM'
     },
     {
-        description:"Eigth Rover",
+        description:"Ninth Rover",
         limits: "5,5",
         roverStart: "5,5,W",
         roverMovements: 'LLMMMMM'
+    },
+    {
+        description:"Tenth Rover (starts outside boundary)",
+        limits: "5,5",
+        roverStart: "10,10,W",
+        roverMovements: 'LLMMMMM'
+    },
+
+
+]
+invalidInputTests = [
+    {
+        description:"Eleventh Rover",
+        limits: "5,5",
+        roverStart: "1,2,N",
+        roverMovements: 'RRM123MMMM'
+    },
+    {
+        description:"Tweltfth Rover",
+        limits: "5,5",
+        roverStart: "1,2,N",
+        roverMovements: 'RRAMMCMMM'
+    },
+    {
+        description:"Thirteenth Rover",
+        limits: "0,0",
+        roverStart: "1,2,N",
+        roverMovements: 'RRMMMMM'
+    },
+    {
+        description:"Fourteenth Rover",
+        limits: "",
+        roverStart: "1,2,N",
+        roverMovements: 'RRMMMMM'
+    },
+    {
+        description:"Fifteenth Rover",
+        limits: "X,9",
+        roverStart: "1,2,N",
+        roverMovements: 'RRMMMMM'
     },
 
 
@@ -76,10 +123,13 @@ describe("testCases", ()=>{
 
         })
     })
-
-
-
-    // expect().toThrow(Error)
+    describe("invalid input tests", ()=>{
+        invalidInputTests.forEach(({description , limits , roverStart , roverMovements})=>{
+            test(description, ()=>{
+                expect(()=>rover(limits, startPosition = roverStart, movements = roverMovements).toThrow(Error))
+            })
+        })
+    })
 } )
 
 
